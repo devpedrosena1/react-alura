8fde2703f6d4c944393b1fd39c486f60fedac196
devpedrosena1 <devpedrosena1@gmail.com>
2025-09-09 19:31:30 -0300

    continuando estilização image e view

--- fokus/app/index.jsx
+++ fokus/app/index.jsx
@@ -1,23 +1,31 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={styles.container}
     >
-      <Text style={styles.text}>Edit app/index.jsx to edit this screen.</Text>
+      <Image source={require('./pomodoro.png')}/>
+      <View style={styles.action}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#021123",
-      },
-      text: {
-        color: "#fff",
-      }
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#021123",
+    gap: 40
+  },
+  action: {
+    paddingVertical: 24,
+    paddingHorizontal: 24, 
+    backgroundColor: "#14448080",
+    width: "80%",
+    borderRadius: 34,
+    borderWidth: 2,
+    borderColor: "#144480"
+  }
 })
